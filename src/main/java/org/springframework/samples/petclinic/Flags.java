4bbc2eb3e3030fb40f3c64020e39a5e735c2dd72
johns2te <johns2te@gmail.com>
2024-11-22 15:45:59 -0500

    CBDEMO-12

--- src/main/java/org/springframework/samples/petclinic/Flags.java
+++ src/main/java/org/springframework/samples/petclinic/Flags.java
@@ -7,11 +7,11 @@ public class Flags implements RoxContainer {
 
 	public RoxFlag enableTutorial = new RoxFlag(false);
 
-	public final RoxString titleColors = new RoxString("White", new String[] { "White", "Blue", "Green", "Yellow" });
+	public final RoxString titleColors = new RoxString("Blue", new String[] { "White", "Blue", "Green", "Yellow" });
 
 	public final RoxString shozabColors = new RoxString("White", new String[] { "White", "Blue", "Green", "Yellow" });
 
-	public final RoxInt titleSize = new RoxInt(14, new int[] { 14, 18, 3 });
+	public final RoxInt titleSize = new RoxInt(3, new int[] { 14, 18, 3 });
 
 	public final RoxDouble specialNumber = new RoxDouble(3.140000, new double[] { 2.71, 0.577 });
 
